6dc86bbf9fdf7dfa4b764f9e6ec501907b2c66a9
Patryk Pekala <ppekala@icloud.com>
2020-09-11 16:10:38 +0200

    Add DbContext

---
+++ DateFinder/Models/UserSelectionContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace DateFinder.Models
+{
+    public class UserSelectionContext : DbContext
+    {
+        public UserSelectionContext(DbContextOptions<UserSelectionContext> options) : base(options)
+        {
+        }
+
+        public DbSet<UserSelection> UserSelections { get; set; }
+    }
+}
