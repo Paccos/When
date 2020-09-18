0fced66fec05f53def4921d49d11a0eb848875c2
Patryk Pekala <ppekala@icloud.com>
2020-09-18 17:21:45 +0200

    Add localized DayPicker component, manage date selection and post a new poll on submit click

--- DateFinder/ClientApp/src/components/NewPoll.js
+++ DateFinder/ClientApp/src/components/NewPoll.js
@@ -1,9 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
 import './NewPoll.css';
 import SubmitButton from './SubmitButton';
 
+import DayPicker, { DateUtils } from 'react-day-picker';
+import 'react-day-picker/lib/style.css';
+import MomentLocaleUtils from 'react-day-picker/moment';
+import 'moment/locale/de';
+
 export const NewPoll = (props) => {
+	const [authorName, setAuthorName] = useState('');
+	const [pollTitle, setPollTitle] = useState('');
+	const [selectedDays, setSelectedDays] = useState([]);
+
+	const history = useHistory();
+
+	const onDaySelect = (day) => {
+		let selection = [...selectedDays];
+		const selectionIndex = selection.findIndex((date) =>
+			DateUtils.isSameDay(date, day)
+		);
+
+		if (selectionIndex === -1) selection.push(day);
+		else selection.splice(selectionIndex, 1);
+
+		setSelectedDays(selection);
+	};
+
+	const postPoll = () => {
+		const dateSelections = selectedDays.map((date) => ({
+			date: date,
+			state: 1,
+		}));
+
+		const poll = {
+			author: authorName,
+			title: pollTitle,
+			userSelections: [{ name: authorName, dateSelections: dateSelections }],
+		};
+
+		const requestOptions = {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify(poll),
+		};
+
+		console.log(requestOptions.body);
+
+		fetch('api/polls', requestOptions)
+			.then((response) => response.json())
+			.then((data) => {
+				console.log(data);
+
+				const pollId = data.id;
+
+				history.push(`/${pollId}`);
+			});
+	};
+
 	return (
 		<div className="main">
 			<div>
@@ -11,9 +66,17 @@ export const NewPoll = (props) => {
 
 				<div className="formFields">
 					<label>Dein Name:</label>
-					<input type="text" placeholder="Max Mustermann" />
+					<input
+						type="text"
+						placeholder="Max Mustermann"
+						onChange={(e) => setAuthorName(e.target.value)}
+					/>
 					<label>Titel der Umfrage:</label>
-					<input type="text" placeholder="Star Wars Marathon" />
+					<input
+						type="text"
+						placeholder="Star Wars Marathon"
+						onChange={(e) => setPollTitle(e.target.value)}
+					/>
 				</div>
 			</div>
 
@@ -22,10 +85,15 @@ export const NewPoll = (props) => {
 					Tage auswählen:
 				</h3>
 				<h3 className="subtitle" id="chosenDaysCounter">
-					9 Tage ausgewählt
+					{selectedDays.length} Tage ausgewählt
 				</h3>
-				<div className="calendar"></div>
-				<SubmitButton />
+				<DayPicker
+					localeUtils={MomentLocaleUtils}
+					locale="de"
+					selectedDays={selectedDays}
+					onDayClick={onDaySelect}
+				/>
+				<SubmitButton submitHandler={() => postPoll()} />
 			</div>
 		</div>
 	);
