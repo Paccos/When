e053659d18ee2afd2d70f4d1659ff6c2a5dfee49
Patryk Pekala <ppekala@icloud.com>
2020-10-20 17:33:54 +0200

    Finish tests for PollsController and return BadRequest for missing data in PostPoll

--- WhenTests/PollControllerTests.cs
+++ WhenTests/PollControllerTests.cs
@@ -4,25 +4,42 @@ using When.Controllers;
 using Microsoft.EntityFrameworkCore;
 using When.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace WhenTests
 {
     public class PollControllerTests
     {
         private readonly PollsController _pollsControllerToTest;
+        private readonly PollContext _pollsContext;
 
         public PollControllerTests()
         {
             var optionsBuilder = new DbContextOptionsBuilder<PollContext>();
             optionsBuilder.UseInMemoryDatabase("WhenTest");
 
-            var context = new PollContext(optionsBuilder.Options);
+            _pollsContext = new PollContext(optionsBuilder.Options);
+            _pollsControllerToTest = new PollsController(_pollsContext);
+        }
 
-            _pollsControllerToTest = new PollsController(context);
+        private List<UserSelection> TestData()
+        {
+            var dateSelecitons = Enumerable
+                .Range(1, 10)
+                .Select(i => DateTime.Now.AddDays(i))
+                .Select(d => new DateSelection { Date = d, State = SelectionState.No })
+                .ToList();
+
+            return new List<UserSelection> {
+                new UserSelection { Name = "Bob", DateSelections = dateSelecitons },
+                new UserSelection { Name = "Alice", DateSelections = dateSelecitons },
+                new UserSelection { Name = "Chad", DateSelections = dateSelecitons }
+            };
         }
 
         [Fact]
-        public async System.Threading.Tasks.Task GetWithInvalidIdReturnsNotFoundAsync()
+        public async void GetWithInvalidIdReturnsNotFoundAsync()
         {
             Guid id = new Guid();
 
@@ -32,21 +49,69 @@ namespace WhenTests
         }
 
         [Fact]
-        public void GetReturnsPollWithUserSelections()
+        public async void GetReturnsPollWithUserSelections()
         {
-            throw new NotImplementedException();
+            var testPoll = new Poll { Title = "TestPoll", UserSelections = TestData() };
+            await _pollsContext.AddAsync(testPoll);
+
+            var id = testPoll.Id;
+
+            var fetchedPoll = await _pollsControllerToTest.GetPollAsync(id);
+
+            Assert.IsNotType<NotFoundResult>(fetchedPoll.Result);
+
+            Assert.NotNull(fetchedPoll.Value);
+            Assert.NotNull(fetchedPoll.Value.UserSelections);
+
+            Assert.Equal(testPoll.Title, fetchedPoll.Value.Title);
+            Assert.Equal(testPoll.UserSelections, fetchedPoll.Value.UserSelections);
         }
 
         [Fact]
-        public void PostWithoutRequiredDataReturnsBadRequest()
+        public async void PostWithoutRequiredDataReturnsBadRequest()
         {
-            throw new NotImplementedException();
+            // Test with empty data
+            var badPoll = new Poll();
+
+            var result = await _pollsControllerToTest.PostPoll(badPoll);
+
+            Assert.IsType<BadRequestObjectResult>(result.Result);
+
+            // Test with empty title
+            badPoll.UserSelections = TestData();
+
+            result = await _pollsControllerToTest.PostPoll(badPoll);
+
+            Assert.IsType<BadRequestObjectResult>(result.Result);
+
+            // Test with empty UserSelection
+            badPoll.UserSelections = new List<UserSelection>();
+            badPoll.Title = "TestPoll";
+
+            result = await _pollsControllerToTest.PostPoll(badPoll);
+
+            Assert.IsType<BadRequestObjectResult>(result.Result);
         }
 
         [Fact]
-        public void PostReturnsCreatedAtWithAuthorIdAndSortedDateSelections()
+        public async void PostReturnsCreatedAtWithAuthorIdAndSortedDateSelections()
         {
-            throw new NotImplementedException();
+            var testPoll = new Poll { Title = "TestPoll", UserSelections = TestData() };
+
+            var result = await _pollsControllerToTest.PostPoll(testPoll);
+            Assert.IsType<CreatedAtActionResult>(result.Result);
+
+            var createdAtResult = result.Result as CreatedAtActionResult;
+            var postedPoll = createdAtResult.Value as Poll;
+
+            Assert.Equal(postedPoll.UserSelections[0].Id, postedPoll.AuthorId);
+
+            var sortedTestSelections = testPoll.UserSelections
+                .First()
+                .DateSelections
+                .OrderBy(ds => ds.Date).ToList();
+
+            Assert.Equal(sortedTestSelections, postedPoll.UserSelections.First().DateSelections);
         }
     }
 }
